e0fe2e05e0a712678ba7ad290ce7c690aae15f02
SamadjonIslomov <149060290+SamadjonIslomov@users.noreply.github.com>
2024-03-24 15:34:37 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,20 +3,39 @@ const searchButton = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
 const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
+const date = document.querySelector("#current-date");
 const cityInput = document.querySelector(".search-box input");
 const APIKey = "27d7d8bcf5015828dc2efb1b6acd047d";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const cityName = document.querySelector(".city");
+const BtnCity = document.querySelector(".cityBtn");
+const region = document.querySelector(".cityName");
+const regions = [
+  document.querySelector(".region1"),
+  document.querySelector(".region2"),
+  document.querySelector(".region3"),
+  document.querySelector(".region4"),
+  document.querySelector(".region5"),
+  document.querySelector(".region6"),
+  document.querySelector(".region7"),
+  document.querySelector(".region8"),
+  document.querySelector(".region9"),
+  document.querySelector(".region10"),
+  document.querySelector(".region11"),
+  document.querySelector(".region12"),
+];
 
 searchButton.addEventListener("click", searchWeather);
 
 cityInput.addEventListener("keydown", (event) => {
-  if (event.keyCode === 13) {
+  if (event.key === "Enter") {
     searchWeather();
   }
 });
 
 function searchWeather() {
   const city = cityInput.value.trim();
-
   if (city === "") return;
 
   fetch(
@@ -30,6 +49,8 @@ function searchWeather() {
         weatherDetails.style.display = "none";
         error404.style.display = "block";
         error404.classList.add("fadeIn");
+        date.style.display = "none";
+        btn.style.display = "none";
       } else {
         error404.style.display = "none";
         error404.classList.remove("fadeIn");
@@ -56,7 +77,7 @@ function searchWeather() {
             image.src = "cloud.png";
             break;
           case "Haze":
-            image.src = "haze.png";
+            image.src = "mist.png";
             break;
           default:
             image.src = "mist.png";
@@ -64,14 +85,65 @@ function searchWeather() {
 
         temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
         description.innerHTML = json.weather[0].description;
-        humidity.innerHTML = `${json.main.humidity}%`;
+        humidity.innerHTML = `${json.main.humidity}%  `;
         wind.innerHTML = `${parseInt(json.wind.speed)} km/h`;
         weatherBox.style.display = "block";
         weatherDetails.style.display = "flex";
         weatherBox.classList.add("fadeIn");
         weatherDetails.classList.add("fadeIn");
-        container.style.height = "590px";
+        container.style.height = "630px";
+        date.style.display = "block";
+        btn.style.display = "block";
       }
     })
     .catch((error) => console.error("Fetch error:", error));
 }
+document.addEventListener("DOMContentLoaded", function () {
+  showCurrentDate();
+});
+
+function showCurrentDate() {
+  const now = new Date();
+  const dateString = now.toLocaleDateString("uz-UZ", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  document.getElementById("current-date").textContent = dateString;
+}
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    region.style.scale = "0";
+    modal.style.scale = "0";
+  }
+});
+BtnCity.addEventListener("click", () => {
+  modal.style.scale = "1";
+  region.style.scale = "1";
+  regions.forEach((reg) => {
+    reg.style.display = "block";
+  });
+});
+modal.addEventListener("click", () => {
+  modal.style.scale = "0";
+  region.style.scale = "0";
+});
+// Regions uchun event listener qo'shish
+function setupRegionClicks() {
+  regions.forEach((region) => {
+    region.addEventListener("click", function () {
+      cityInput.value = this.textContent;
+      modal.style.scale = "0";
+      regions.forEach((reg) => {
+        reg.style.display = "none";
+      });
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setupRegionClicks();
+  showCurrentDate();
+});
